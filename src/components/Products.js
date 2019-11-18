cf49194629ff57e24317b8694bd418d883057b38
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-11-19 00:03:50 +0700

    util function

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react"; 
+import util from '../util';
 
 export default class Products extends Component {
   render() {
       const productsItems = this.props.products.map( product => (
-          <div className="col-md-4">
+          <div key={ product.id } className="col-md-4">
             <div className="thumbnail text-center">
-                <a href={`#${product.id}`} onClick={this.props.handleAddToCart}>
-                    <img src={`/products/${product.sku}.jpg`} alt={product.title} />
+                <a href={`#${product.id}`} onClick={(e) => this.props.handleAddToCart(e, product)}>
+                    <img src={`/products/${product.sku}_2.jpg`} alt={product.title} />
                     <p>
                         { product.title }
                     </p>
                 </a>
+                <b>{ util.formatCurrency(product.price) } </b>
+                <button className="btn btn-primary" onClick={ (e) => this.props.handleAddToCart(e, product) } >Add To Cart</button>
             </div>
           </div>
       ));
